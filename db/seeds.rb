f89ea725705f536c84bf3bc374a9b2bf1535f3cb
mahmoud moursy <mahmoud.salah.rabee@gmail.com>
2024-02-02 13:55:01 +0100

    Articles page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+# Clear existing records
+Article.destroy_all
+
+# Create 10 fake articles
+10.times do
+  Article.create(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraphs.join("\n\n")
+  )
+end
